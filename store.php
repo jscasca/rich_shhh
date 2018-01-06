e1c5cfaab3525bdcf11d133fff7e011b32122f85
jscasca <jscasca@gmail.com>
2018-01-07 00:17:53 +1100

    Save stuff

---
+++ store.php
@@ -0,0 +1,11 @@
+<?php
+//do all sorts of crazy stuff
+
+//get the id = hash( user + name)
+
+//look for the element in db
+
+// if found email them that it failed
+
+//if not found store this new one and email them that 
+?>
