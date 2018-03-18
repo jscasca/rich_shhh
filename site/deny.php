86db72780ae47f136a37ccc4f8fc8d3e144cb89f
jscasca <jscasca@gmail.com>
2018-03-18 17:10:59 +1100

    Convert to pseudo aPI call

--- site/deny.php
+++ site/deny.php
@@ -1,5 +1,8 @@
 <?php
 //MAKE A CLAIM
+header('Access-Control-Allow-Origin: http://localhost:3000', false);
+header('Access-Control-Allow-Methods: POST,GET', false);
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept', false);
 define('MAX_ACCESS_LOG', 6);
 
 require('../includes/Mailer.php');
@@ -17,8 +20,8 @@ function gone() {
 }
 
 $code = $_REQUEST['code']; //Name is the name of the contents being stored
-if(!Util::validateCode($trustee)) { //Must not be empty
-	fail();
+if(!Util::validateCode($code)) { //Must not be empty
+	header("HTTP/1.1 400 Bad Request");die();
 }
 
 $con = new Congo();
@@ -36,12 +39,14 @@ if($q->hasResults()) {
 	$mailer->notifyOwnerAboutDenial($user, $code);
 	$mailer->notifyTrusteeAboutDenial($trustee);
 	//Notify owner
+	header("HTTP/1.1 200 OK");
 } else {
 	//Resource does not exist
 	//gone(); //log and notify if needed
+	header("HTTP/1.1 410 Gone");die();
 }
 
-header("Location: http://localhost:3000/claim?i=".$_REQUEST['i']."&code=".$_REQUEST['code']);
+//header("Location: http://localhost:3000/claim?i=".$_REQUEST['i']."&code=".$_REQUEST['code']);
 
 //header("HTTP/1.1 200 OK");
 
